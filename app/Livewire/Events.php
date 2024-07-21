ce80f87aa89fb399229621ca57e94ef444569d71
mnasdar <mnasdar@gmail.com>
2024-07-22 00:20:04 +0800

    Create Add Event, Edit Event and UI table Event

--- app/Livewire/Events.php
+++ app/Livewire/Events.php
@@ -5,33 +5,121 @@ namespace App\Livewire;
 use App\Models\Event;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Carbon;
 
 class Events extends Component
 {
     use WithPagination;
+
     public $active = false;
-    public $q;
+    public $search;
+    public $sortBy = 'id';
+    public $sortAsc = true;
     public $confirmingEventDeletion = false;
+    public $confirmingEventAdd = false;
+    public $event;
+    public $form = [
+        'name' => '',
+        'category' => '',
+        'description' => '',
+        'start_date' => '',
+        'end_date' => '',
+        'location' => '',
+    ];
+
+    protected $queryString = [
+        'active' => ['except' => false],
+        'search' => ['except' => ''],
+        'sortBy' => ['except' => 'created_at'],
+        'sortAsc' => ['except' => true],
+    ];
+
+    public function rules()
+    {
+        return [
+            'form.name' => 'required|min:5|string',
+            'form.category' => 'required',
+            'form.description' => 'required|min:5',
+            'form.start_date' => 'required|date|after:tomorrow',
+            'form.end_date' => 'required|date|after:form.start_date',
+            'form.location' => 'required|min:5|string',
+        ];
+    }
+
+    public function mount()
+    {
+        $this->event = null;
+    }
+
     public function render()
     {
         $events = Event::where('user_id', auth()->user()->id)
-            ->when($this->q, function ($query){
-                return $query->where(function ($query){
-                    $query->where('name', 'like', '%'.$this->q.'%')
-                          ->orWhere('category','like','%'.$this->q.'%')
-                          ->orWhere('start_date','like','%'.$this->q.'%')
-                          ->orWhere('end_date','like','%'.$this->q.'%')
-                          ->orWhere('location','like','%'.$this->q.'%');
-            });
+            ->when($this->search, function ($query) {
+                return $query->where(function ($query) {
+                    $query->where('name', 'like', '%' . $this->search . '%')
+                        ->orWhere('category', 'like', '%' . $this->search . '%')
+                        ->orWhere('start_date', 'like', '%' . $this->search . '%')
+                        ->orWhere('end_date', 'like', '%' . $this->search . '%')
+                        ->orWhere('location', 'like', '%' . $this->search . '%');
+                });
             })
             ->when($this->active, function ($query) {
                 return $query->where('status', 1);
-            });
+            })
+            ->orderBy($this->sortBy, $this->sortAsc ? 'Asc' : 'Desc')
+            ->paginate(10);
 
-        $query = $events->tosql();
-        $events = $events->paginate(10);
+        return view('livewire.events', compact('events'));
+    }
 
-        return view('livewire.events', compact(['events', 'query']));
+    public function orderBy($field)
+    {
+        if ($field == $this->sortBy) {
+            $this->sortAsc = !$this->sortAsc;
+        } else {
+            $this->sortAsc = true;
+        }
+        $this->sortBy = $field;
+    }
+
+    public function saveEvent()
+    {
+        $validated = $this->validate();
+        // Convert the dates back to the format expected by the database
+        $validated['form']['start_date'] = Carbon::parse($validated['form']['start_date'])->format('Y-m-d');
+        $validated['form']['end_date'] = Carbon::parse($validated['form']['end_date'])->format('Y-m-d');
+
+        if ($this->event) {
+            // Update existing event
+            $this->event->update($validated['form']);
+            session()->flash('success', 'Event successfully updated.');
+        } else {
+            // Create new event
+            $validated['form']['user_id'] = auth()->user()->id;
+            $validated['form']['status'] = 1;
+
+            Event::create($validated['form']);
+            session()->flash('success', 'Event successfully saved.');
+        }
+
+        $this->resetForm();
+        $this->confirmingEventAdd = false;
+    }
+
+    public function confirmEventAdd()
+    {
+        $this->resetForm();
+        $this->confirmingEventAdd = true;
+    }
+
+    public function confirmEventEdit(Event $event)
+    {
+        $this->event = $event;
+        $this->form = $event->toArray();
+        // Format the dates to Y-m-d for the form input fields
+        $this->form['start_date'] = Carbon::parse($event->start_date)->format('Y-m-d');
+        $this->form['end_date'] = Carbon::parse($event->end_date)->format('Y-m-d');
+        $this->confirmingEventAdd = true;
     }
 
     public function confirmEventDeletion($id)
@@ -43,5 +131,18 @@ class Events extends Component
     {
         $event->delete();
         $this->confirmingEventDeletion = false;
+    }
+
+    private function resetForm()
+    {
+        $this->form = [
+            'name' => '',
+            'category' => '',
+            'description' => '',
+            'start_date' => '',
+            'end_date' => '',
+            'location' => '',
+        ];
+        $this->event = null;
     }
 }
--- resources/views/livewire/events.blade.php
+++ resources/views/livewire/events.blade.php
@@ -1,33 +1,167 @@
 <div
     class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
-    <div class="flex justify-between">
+    <div class="mt-5 text-2xl flex justify-between">
+        <div>Events</div>
+        <div class="mr-2">
+            <x-button-primary wire:click="confirmEventAdd">
+                Add New Event
+            </x-button-primary>
+        </div>
+        <!-- Add Event Confirmation Modal -->
+        <x-dialog-modal wire:model.live="confirmingEventAdd">
+            <x-slot name="title">
+                {{ $event ? 'Edit Event' : 'Add Event' }}
+            </x-slot>
+
+            <form wire:submit="saveEvent">
+                <x-slot name="content">
+                    <div class="mt-4 col-span-6 sm:col-span-4">
+                        <x-label for="name" value="{{ __('Name') }}" />
+                        <x-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="form.name" />
+                        <x-input-error for="form.name" class="mt-2" />
+                    </div>
+                    <div class="mt-4 col-span-6 sm:col-span-4">
+                        <x-label for="category" value="{{ __('Category') }}" />
+                        <x-select id="category" class="mt-1 block w-full" wire:model.defer="form.category">
+                            <option value="">Pilih</option>
+                            <option value="Music">Music</option>
+                            <option value="Festival">Festival</option>
+                            <option value="Food Court">Food Court</option>
+                        </x-select>
+                        <x-input-error for="form.category" class="mt-2" />
+                    </div>
+                    <div class="mt-4 col-span-6 sm:col-span-4">
+                        <x-label for="description" value="{{ __('Description') }}" />
+                        <x-text-area id="description" class="mt-1 block w-full" wire:model.defer="form.description" />
+                        <x-input-error for="form.description" class="mt-2" />
+                    </div>
+                    <div class="grid grid-cols-12 mt-4 sm:col-span-4">
+                        <div class="col-span-6">
+                            <x-label for="start_date" value="{{ __('Start Date') }}" />
+                            <x-input id="start_date" type="date" class="mt-1 block w-50"
+                                wire:model.defer="form.start_date" />
+                            <x-input-error for="form.start_date" class="mt-2" />
+                        </div>
+                        <div class="col-span-6">
+                            <x-label for="end_date" value="{{ __('End Date') }}" />
+                            <x-input id="end_date" type="date" class="mt-1 block w-50" wire:model.defer="form.end_date" />
+                            <x-input-error for="form.end_date" class="mt-2" />
+                        </div>
+                    </div>
+                    <div class="mt-4 col-span-6 sm:col-span-4">
+                        <x-label for="location" value="{{ __('Location') }}" />
+                        <x-input id="location" type="text" class="mt-1 block w-full" wire:model.defer="form.location" />
+                        <x-input-error for="form.location" class="mt-2" />
+                    </div>
+                </x-slot>
+
+                <x-slot name="footer">
+                    <x-secondary-button wire:click="$set('confirmingEventAdd',false)" wire:loading.attr="disabled">
+                        {{ __('Cancel') }}
+                    </x-secondary-button>
+
+                    <x-button-save class="ms-3" wire:click="saveEvent()" wire:loading.attr="disabled">
+                        {{ __('Save') }}
+                        </x-danger-button>
+
+                </x-slot>
+            </form>
+        </x-dialog-modal>
+    </div>
+    <div class="mt-5 flex justify-between">
         <div class="p-2">
-            <input wire:model.live.debounce.250ms="q" type="search" placeholder="" class="text-gray-700 shadow appearance-none border rounded w-full">
+            <input wire:model.live.debounce.250ms="search" type="search" placeholder=""
+                class="bg-slate-800 text-gray-300 shadow appearance-none border rounded w-full">
         </div>
         <div class="mr-2">
-            <input type="checkbox" class="border rounded mr-2 leading-tight" wire:model.live="active" /> Active Only
+            <input type="checkbox" class="bg-slate-800 border rounded mr-2 leading-tight" wire:model.live="active" />
+            Active Only
         </div>
     </div>
     <table class="mt-3 min-w-full divide-y divide-gray-200 dark:divide-gray-700">
         <thead class="bg-gray-50 dark:bg-gray-700">
             <tr>
-                <th class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider">No.</th>
-                <th class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider">Name</th>
-                <th class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider">Category
+                <th class="px-4 py-2 text-left tracking-wider whitespace-nowrap inline-block">
+                    <div class="grid grid-cols-2">
+                        <div class="content-center">
+                            <button type="button" wire:click="orderBy('id')"
+                                class="text-sm font-medium text-gray-300 uppercase">No
+                            </button>
+                        </div>
+                        <div class="ms-2">
+                            <x-sort-icon sortField="id" :sort-by="$sortBy" :sort-asc="$sortAsc" />
+                        </div>
+                    </div>
+                </th>
+                <th class="px-4 py-2 text-left tracking-wider whitespace-nowrap">
+                    <div class="grid grid-cols-2">
+                        <div class="content-center">
+                            <button type="button" wire:click="orderBy('name')"
+                                class="text-sm font-medium text-gray-300 uppercase">Name
+                            </button>
+                        </div>
+                        <div class="ms-2">
+                            <x-sort-icon sortField="name" :sort-by="$sortBy" :sort-asc="$sortAsc" />
+                        </div>
+                    </div>
+                </th>
+                <th class="px-4 py-2 text-left tracking-wider whitespace-nowrap">
+                    <div class="grid grid-cols-2">
+                        <div class="content-center">
+                            <button type="button" wire:click="orderBy('category')"
+                                class="text-sm font-medium text-gray-300 uppercase">Category
+                            </button>
+                        </div>
+                        <div class="ms-2">
+                            <x-sort-icon sortField="category" :sort-by="$sortBy" :sort-asc="$sortAsc" />
+                        </div>
+                    </div>
+                </th>
+                <th class="px-4 py-2 text-left tracking-wider whitespace-nowrap">
+                    <div class="grid grid-cols-2">
+                        <div class="content-center">
+                            <button type="button" wire:click="orderBy('start_date')"
+                                class="text-sm font-medium text-gray-300 uppercase">Start Date
+                            </button>
+                        </div>
+                        <div class="ms-2">
+                            <x-sort-icon sortField="start_date" :sort-by="$sortBy" :sort-asc="$sortAsc" />
+                        </div>
+                    </div>
+                </th>
+                <th class="px-4 py-2 text-left tracking-wider whitespace-nowrap">
+                    <div class="grid grid-cols-2">
+                        <div class="content-center">
+                            <button type="button" wire:click="orderBy('end_date')"
+                                class="text-sm font-medium text-gray-300 uppercase">End Date
+                            </button>
+                        </div>
+                        <div class="ms-2">
+                            <x-sort-icon sortField="end_date" :sort-by="$sortBy" :sort-asc="$sortAsc" />
+                        </div>
+                    </div>
+                </th>
+                <th class="px-4 py-2 text-left tracking-wider">
+                    <div class="grid grid-cols-2">
+                        <div class="content-center">
+                            <button type="button" wire:click="orderBy('location')"
+                                class="text-sm font-medium text-gray-300 uppercase">Location
+                            </button>
+                        </div>
+                        <div class="ms-2">
+                            <x-sort-icon sortField="location" :sort-by="$sortBy" :sort-asc="$sortAsc" />
+                        </div>
+                    </div>
+                </th>
+                @if (!$active)
+                    <th
+                        class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider whitespace-nowrap">
+                        Status
+                @endif
                 </th>
                 <th
                     class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider whitespace-nowrap">
-                    Start Date
-                </th>
-                <th
-                    class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider whitespace-nowrap">
-                    End Date
-                </th>
-                <th class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider">Location
-                </th>
-                <th class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider">Status
-                </th>
-                <th class="px-4 py-2 text-left text-sm font-medium text-gray-300 uppercase tracking-wider">Action
+                    Action
                 </th>
             </tr>
         </thead>
@@ -40,22 +174,22 @@
                     <td class="px-4 py-2 whitespace-nowrap">{{ $event->start_date }}</td>
                     <td class="px-4 py-2 whitespace-nowrap">{{ $event->end_date }}</td>
                     <td class="px-4 py-2 whitespace">{{ $event->location }}</td>
-                    <td class="px-4 py-2 whitespace-nowrap">{{ $event->status ? 'Active' : 'Not Active' }}</td>
+                    @if (!$active)
+                        <td class="px-4 py-2 whitespace-nowrap">{{ $event->status ? 'Active' : 'Not Active' }}</td>
+                    @endif
                     <td class="px-4 py-2 whitespace-nowrap">
-                        <x-info-button wire:click="confirmEventDeletion({{ $event->id }})"
+                        <x-button-info wire:click="confirmEventEdit({{ $event->id }})"
                             wire:loading.attr="disabled">
-                            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                             </svg>
-
-
-                        </x-info-button>
+                        </x-button-info>
                         <x-danger-button wire:click="confirmEventDeletion({{ $event->id }})"
                             wire:loading.attr="disabled">
-                            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
@@ -66,10 +200,10 @@
             @endforeach
         </tbody>
     </table>
-    <div>
+    <div class="mt-4">
         {{ $events->links() }}
     </div>
-    <!-- Delete User Confirmation Modal -->
+    <!-- Delete Event Confirmation Modal -->
     <x-dialog-modal wire:model.live="confirmingEventDeletion">
         <x-slot name="title">
             {{ __('Delete') }}
@@ -84,7 +218,8 @@
                 {{ __('Cancel') }}
             </x-secondary-button>
 
-            <x-danger-button class="ms-3" wire:click="deleteEvent({{ $confirmingEventDeletion }})" wire:loading.attr="disabled">
+            <x-danger-button class="ms-3" wire:click="deleteEvent({{ $confirmingEventDeletion }})"
+                wire:loading.attr="disabled">
                 {{ __('Delete') }}
             </x-danger-button>
         </x-slot>
